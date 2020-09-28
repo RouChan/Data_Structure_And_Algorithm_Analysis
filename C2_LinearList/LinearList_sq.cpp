cd4a8d3280380a8c39bcc4f80370e34e58593d4e
Chan <fishlikeorangesunny@outlook.com>
2020-09-28 23:35:13 +0800

    W5: 1. 添加顺序表的头文件；

--- C2_LinearList/LinearList_sq.cpp
+++ C2_LinearList/LinearList_sq.cpp
@@ -5,93 +5,17 @@
 
 #include <iostream>
 #include <string>
-
-#define DEFAULT_SIZE 10
+#include "../Header&Library/SeqList.h"
 using namespace std;
 
-struct Student {
-    string name;
-    int age;
-};
-
-class SqList{
-protected:
-    int count;                                      // 当前元素个数&最后一个元素后一个数组位置编号
-    int maxSize;                                    // 初始化预留的最大顺序表元素个数
-    Student* stu;
-public:
-    SqList(int size = DEFAULT_SIZE);                // 声明和定义分离，避免野指针
-    void Init(int size);                            // 设置顺序表元素内容
-    ~SqList() {delete[] stu;};                      // 析构函数
-
-    void Clear();                                   // 清空顺序表
-    void AddStu(string name, int age);              // 在末尾添加元素
-    void SetStu(int loc, string name, int age);     // 修改特定位置元素
-
-    int Length() {return count;};                   // 返回顺序表元素个数
-    bool IsEmpty() {return count;};                 // 判断顺序表是否为空
-
-    void GetStu(int loc);                           // 取元素信息
-    void Traverse();                                // 遍历
-};
-
-SqList::SqList(int size) {
-    stu = nullptr;
-    Init(size);
-}
-
-void SqList::Init(int size) {
-    maxSize = size;
-    if(stu != nullptr) delete[] stu;
-    stu = new Student[size];
-    count = 0;
-}
-
-void SqList::Clear() {
-    count = 0;
-    maxSize = 0;
-    if(stu != nullptr) {
-        delete[] stu;
-        stu = nullptr;
-    }
-}
-
-void SqList::Traverse() {
-    for(int i = 0; i < count; ++i) {
-        GetStu(i);
-    }
-    cout << endl;
-}
-
-void SqList::AddStu(string name, int age)  {
-    if(count >= maxSize) {
-        cerr << "插入失败，超过最大容量限制!" << endl;
-//        exit(1);
-        return ;
-    } else {
-        stu[count].name = name;
-        stu[count].age = age;
-        count++;
-        return;
-    }
-}
-
-
-void SqList::SetStu(int loc, string name, int age) {
-    stu[loc].name = name;
-    stu[loc].age = age;
-    return ;
-}
-
-void SqList::GetStu(int i) {
-    cout << stu[i].name << " " << stu[i].age << endl;
-}
-
 int main () {
-    SqList CS_1(5);
-    CS_1.AddStu("Wang", 20);
+    SeqList<int> stuAge(5);
 
-    CS_1.Traverse();
+    for(int i = 0; i < 3; i++) {
+        stuAge.Insert(stuAge.Length() + 1, i);
+    }
+
+    stuAge.Traverse();
 
     return 0;
 }
---
+++ Header&Library/SeqList.h
@@ -0,0 +1,162 @@
+//
+// Created by 喻如辰 on 2020/9/28.
+//
+
+#ifndef DATA_STRUCTURE_AND_ALGORITHM_ANALYSIS_SEQLIST_H
+#define DATA_STRUCTURE_AND_ALGORITHM_ANALYSIS_SEQLIST_H
+
+enum StatusCode {SUCCESS = 0, RANGE_ERROR};
+
+template <class DataType>
+class SeqList {
+protected:
+    DataType*   array;                                      // 元素存储空间
+    int         count;                                      // 元素个数
+    int         maxSize;                                    // 最大元素个数
+
+    bool        Full() const;                               // 判断顺序表是否已满
+    void        Init(int size);                             // 初始化顺序表
+public:
+    SeqList(int size);                                      // 声明、定义一个顺序表
+    virtual ~SeqList();                                     // 析构函数
+
+    int     Length() const;                                 // 返回顺序表的长度
+    bool    Empty() const;                                  // 判断顺序表是否为空
+    void    Clear();                                        // 清空顺序表
+    void    Traverse() const;                               // 遍历
+
+    StatusCode GetElem(int position, DataType& d) const;    // 求指定位置的元素，并存储到d中
+    StatusCode SetElem(int position, const DataType& d);    // 设定position位置的元素为d
+    StatusCode Delete (int position, DataType& d);          // 删除position位置元素，并将的d赋值为被删除元素值
+    StatusCode Insert (int position, DataType& d);          // 在position位置前插入元素d
+
+    SeqList<DataType>& operator = (const SeqList<DataType>& copy);  // 重载操作符
+};
+
+template <class DataType>
+bool SeqList<DataType>::Full() const {
+    return (count == maxSize);
+}
+
+template <class DataType>
+void SeqList<DataType>::Init(int size) {
+    count = 0;
+    maxSize = size;
+    if(array != nullptr) {
+        delete []array;
+    }
+    array = new DataType[size];
+}
+
+template <class DataType>
+SeqList<DataType>::SeqList(int size) {
+    array = nullptr;
+    Init(size);
+}
+
+template <class DataType>
+SeqList<DataType>::~SeqList(){
+    delete []array;
+    array = nullptr;
+}
+
+template <class DataType>
+int SeqList<DataType>::Length() const {
+    return count;
+}
+
+template <class DataType>
+bool SeqList<DataType>::Empty() const {
+    return (count == 0);
+}
+
+template <class DataType>
+void SeqList<DataType>::Clear() {
+    delete []array;
+    array = nullptr;
+    count = 0;
+}
+
+template <class DataType>
+void SeqList<DataType>::Traverse() const {
+    for(int i = 0; i < Length(); i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+template <class DataType>
+StatusCode SeqList<DataType>::GetElem(int position, DataType &d) const{
+    if(position < 1 || position > Length()) {
+        return RANGE_ERROR;
+    } else {
+        d = array[position - 1];
+        return SUCCESS;
+    }
+}
+
+template <class DataType>
+StatusCode SeqList<DataType>::SetElem(int position, const DataType &d) {
+    if(position < 1 || position > Length()) {
+        return RANGE_ERROR;
+    } else {
+        array[position - 1] = d;
+        return SUCCESS;
+    }
+}
+
+template <class DataType>
+StatusCode SeqList<DataType>::Delete(int position, DataType &d) {
+    if(position < 1 || position > Length()) {
+        return RANGE_ERROR;
+    } else {
+        GetElem(position, d);
+
+        DataType tmp;
+        int len = Length();
+        for(int curPosition = position + 1; curPosition <= len; curPosition++) {
+            GetElem(curPosition, tmp);
+            SetElem(curPosition - 1, tmp);
+        }
+        count--;
+        return SUCCESS;
+    }
+}
+
+template <class DataType>
+StatusCode SeqList<DataType>::Insert(int position, DataType &d) {
+    if(Full()) {
+        return RANGE_ERROR;
+    } else if(position < 1 || position > Length() + 1) {
+        return RANGE_ERROR;
+    } else {
+        count++;
+
+        int len = Length();
+        for(int curPosition = len; curPosition >= position; curPosition--) {
+            DataType tmp;
+            GetElem(curPosition, tmp);
+            SetElem(curPosition + 1, tmp);
+
+            SetElem(position, d);
+            return SUCCESS;
+        }
+    }
+}
+
+template <class DataType>
+SeqList<DataType>& SeqList<DataType>::operator = (const SeqList<DataType> &copy) {
+    if (&this != copy) {
+        Insert(copy.maxSize);
+        int len = copy.Length();
+
+        for(int curPosition = 1; curPosition <= len; curPosition++) {
+            DataType tmp;
+            copy.GetElem(curPosition, tmp);
+            Insert(Length() + 1, tmp);
+        }
+        return *this;
+    }
+}
+
+#endif //DATA_STRUCTURE_AND_ALGORITHM_ANALYSIS_SEQLIST_H
