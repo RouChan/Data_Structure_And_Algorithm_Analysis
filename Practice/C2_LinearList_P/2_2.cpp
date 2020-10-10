d166c08e37a7a883703675ed166b06faf68e7c81
Chan <fishlikeorangesunny@outlook.com>
2020-10-10 11:43:09 +0800

    Data Oct.10 1. 搭建框架；

---
+++ Practice/C2_LinearList_P/2_2.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 喻如辰 on 2020/10/10.
+// 第二章线性表
+// 2. 实现一元多项式的表示与运算
+//
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    return 0;
+}
---
+++ Practice/C2_LinearList_P/Polynomial.h
@@ -0,0 +1,24 @@
+//
+// Created by 喻如辰 on 2020/10/10.
+//
+
+#ifndef DATA_STRUCTURE_AND_ALGORITHM_ANALYSIS_POLYNOMIAL_H
+#define DATA_STRUCTURE_AND_ALGORITHM_ANALYSIS_POLYNOMIAL_H
+
+#include "/Users/yu_ruchen/Library/Mobile Documents/com~apple~CloudDocs/Programming/Data_Structure_And_Algorithm_Analysis/Header&Library/SinglyLinkedList.h"
+
+struct PolyItem {
+    int     expo;                                                   // 指数
+    int     coef;                                                   // 系数
+
+    friend ostream& operator<<(ostream& out, PolyItem const s);     // 操作符<<重载
+};
+
+template <class DataType>
+class Polynomial : public List<DataType> {
+public:
+    Polynomial();
+};
+
+
+#endif //DATA_STRUCTURE_AND_ALGORITHM_ANALYSIS_POLYNOMIAL_H
